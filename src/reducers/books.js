ccaa6d2c0f0516ae9faa92a3b689d3f569ec0fc0
Ivan <fivanunam@gmail.com>
2020-06-02 15:34:37 -0500

    add default state

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,6 +1,28 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
-const books = (state = [], action) => {
+const initialState = [
+  {
+    title: 'The power of habit',
+    category: 'self improvement',
+    id: Math.floor(Math.random() * 1000)
+  },
+  {
+    title: 'The prince',
+    category: 'Politics',
+    id: Math.floor(Math.random() * 1000)
+  },
+  {
+    title: 'The everything store',
+    category: 'Biography',
+    id: Math.floor(Math.random() * 1000)
+  },
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    category: 'Sci-Fi',
+    id: Math.floor(Math.random() * 1000)
+  }
+];
+const books = (state = initialState, action) => {
   switch(action.type) {
     case CREATE_BOOK:
       return [...state, action.book];
@@ -15,3 +37,5 @@ const books = (state = [], action) => {
       return state;
   }
 }
+
+export default books;
