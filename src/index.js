7ce309008d33aa84c27c9e3ad6be95f7730cd243
Ivan <fivanunam@gmail.com>
2020-06-02 15:36:12 -0500

    wrapp App component in Provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import books from './reducers/books';
+
+const store = createStore(books);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
