4e3325b3fd303a5a25d03c68ef4362a33b03cc36
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-06-03 17:49:00 -0300

    Style the add book form

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { createBook } from '../actions/index';
 
 const BOOK_CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Politics', 'Sci-Fi', 'Self Improvement'];
@@ -25,7 +26,8 @@ class BooksForm extends React.Component {
 
   handleSubmit(element) {
     element.preventDefault();
-    const { title, category, submitNewBook } = this.state;
+    const { submitNewBook } = this.props;
+    const { title, category } = this.state;
     submitNewBook({
       title,
       category,
@@ -40,19 +42,26 @@ class BooksForm extends React.Component {
   render() {
     const { title, category } = this.state;
     return (
-      <form>
-        <input id="title" onChange={this.handleChange} type="text" value={title} />
-        <select id="category" value={category} onChange={this.handleChange}>
-          {
-            BOOK_CATEGORIES
-              .map(category => <option key={category} value={category}>{category}</option>)
-          }
-        </select>
-        <button type="submit" onClick={this.handleSubmit}>Submit</button>
-      </form>
+      <div className="add-book-container">
+        <h2 className="add-book-header">ADD NEW BOOK</h2>
+        <form className="add-book-form">
+          <input className="add-book-title" placeholder="Book title" id="title" onChange={this.handleChange} type="text" value={title} />
+          <select className="select-generic" id="category" value={category} onChange={this.handleChange}>
+            {
+              BOOK_CATEGORIES
+                .map(category => <option key={category} value={category}>{category}</option>)
+            }
+          </select>
+          <button className="add-book-button" type="submit" onClick={this.handleSubmit}>ADD BOOK</button>
+        </form>
+      </div>
     );
   }
 }
+
+BooksForm.propTypes = {
+  submitNewBook: PropTypes.func.isRequired,
+};
 
 const mapDispatchToProps = dispatch => ({
   submitNewBook: book => {
